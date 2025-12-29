0386ee813e8edc01ae22f6650ae594154dbea897
Osama Selim <osamaselim216@gmail.com>
2025-12-29 23:40:25 +0200

    feat: add product details view, and routing

---
+++ lib/features/product_details/presentation/views/product_details_view.dart
@@ -0,0 +1,18 @@
+import 'package:e_commerce/core/widgets/custom_scaffold.dart';
+import 'package:e_commerce/features/product_details/presentation/widgets/product_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailsView extends StatelessWidget {
+  const ProductDetailsView({super.key});
+
+  static const routeName = '/product-details-view';
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      actionWidgets: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
+      ],
+      child: ProductDetailsViewBody(),
+    );
+  }
+}
---
+++ lib/features/product_details/presentation/widgets/product_details_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerce/core/helpers/constants.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailsViewBody extends StatelessWidget {
+  const ProductDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: Constants.kHorizontalPaddingMedium,
+      child: Column(children: [
+          
+        ],
+      ),
+    );
+  }
+}
