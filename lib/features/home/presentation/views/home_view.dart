63c85d69a874d7b97dfa02f377ff95e1e66f043a
Osama Selim <osamaselim216@gmail.com>
2026-01-22 17:36:44 +0200

    move the profile cubit to be over the material app

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,11 +1,8 @@
-import 'package:e_commerce/core/dependency_injection/di.dart';
 import 'package:e_commerce/core/widgets/custom_scaffold.dart';
 import 'package:e_commerce/features/cart/presentation/views/cart_view.dart';
 import 'package:e_commerce/features/home/presentation/widgets/custom_drawer.dart';
 import 'package:e_commerce/features/home/presentation/widgets/home_view_body.dart';
-import 'package:e_commerce/features/profile/presentation/logic/cubit/get_profile_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class HomeView extends StatelessWidget {
@@ -16,21 +13,18 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: BlocProvider(
-        create: (context) => getIt<GetProfileCubit>()..getProfile(),
-        child: CustomScaffold(
-          title: 'Store',
-          drawer: const CustomDrawer(),
-          actionWidgets: [
-            IconButton(
-              onPressed: () {
-                context.push(CartView.routeName);
-              },
-              icon: const Icon(Icons.shopping_cart_outlined),
-            ),
-          ],
-          child: const HomeViewBody(),
-        ),
+      child: CustomScaffold(
+        title: 'Store',
+        drawer: const CustomDrawer(),
+        actionWidgets: [
+          IconButton(
+            onPressed: () {
+              context.push(CartView.routeName);
+            },
+            icon: const Icon(Icons.shopping_cart_outlined),
+          ),
+        ],
+        child: const HomeViewBody(),
       ),
     );
   }
