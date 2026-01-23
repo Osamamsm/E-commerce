da8fb3d2092ef6d9c698d03d814654d7e470a0b1
Osama Selim <osamaselim216@gmail.com>
2026-01-23 23:39:28 +0200

    implement method for uploading the new user avatar to the avatars bucket in supabase storage

--- lib/features/profile/data/data_source/profile_remote_data_source.dart
+++ lib/features/profile/data/data_source/profile_remote_data_source.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:e_commerce/core/supabase/supabase_service.dart';
 import 'package:e_commerce/features/profile/data/models/user_profile_model.dart';
 import 'package:injectable/injectable.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class ProfileRemoteDataSource {
   Future<UserProfileModel> getUserProfile();
   Future<void> updateProfile(UserProfileModel updatedProfile);
+  Future<String> updateAvatar(File avatar);
 }
 
 @LazySingleton(as: ProfileRemoteDataSource)
@@ -40,5 +44,20 @@ class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
           'phoneNumber': updatedProfile.phoneNumber,
         })
         .eq('id', user.id);
+  }
+
+  @override
+  Future<String> updateAvatar(File avatar) async {
+    final user = _supabaseService.currentUser;
+    if (user == null) {
+      throw Exception('User not found');
+    }
+    return await _supabaseService.storage
+        .from('avatars')
+        .upload(
+          '${user.id}/avatar.jpg',
+          avatar,
+          fileOptions: const FileOptions(cacheControl: '3600', upsert: true),
+        );
   }
 }
