976218cc9cfd7de20f06c04c268e2098fec64f52
Osama Selim <osamaselim216@gmail.com>
2026-02-18 15:56:53 +0200

    implement get address method in address remote data source

--- lib/features/addresses/data/data_sources/address_remote_data_source.dart
+++ lib/features/addresses/data/data_sources/address_remote_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/core/supabase/supabase_service.dart';
 import 'package:e_commerce/features/addresses/data/models/address_model.dart';
 
 abstract class AddressRemoteDataSource {
@@ -6,4 +7,42 @@ abstract class AddressRemoteDataSource {
   Future<void> deleteAddress(String addressId);
   Future<List<AddressModel>> getAddresses();
   Future<void> setDefaultAddress(String addressId);
+}
+
+class AddressRemoteDataSourceImpl implements AddressRemoteDataSource {
+  final SupabaseService _service;
+  AddressRemoteDataSourceImpl(this._service);
+  @override
+  Future<void> addAddress(AddressModel address) {
+    // TODO: implement addAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteAddress(String addressId) {
+    // TODO: implement deleteAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<AddressModel>> getAddresses() async {
+    final user = _service.currentUser;
+    if (user == null) {
+      throw Exception('User not found');
+    }
+    final response = await _service.from('user_address').select().eq('user_id', user.id);
+    return response.map((e) => AddressModel.fromJson(e)).toList();
+  }
+
+  @override
+  Future<void> setDefaultAddress(String addressId) {
+    // TODO: implement setDefaultAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateAddress(AddressModel address) {
+    // TODO: implement updateAddress
+    throw UnimplementedError();
+  }
 }
