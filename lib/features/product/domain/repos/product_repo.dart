ab3a76743f7f9963a9984b63cf2269060c15b6b9
Osama Selim <osamaselim216@gmail.com>
2026-04-07 23:02:20 +0200

    create initial abstract product repo with the main methods and initial models

---
+++ lib/features/product/domain/repos/product_repo.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/error/failure.dart';
+import 'package:e_commerce/features/product/data/models/category.dart';
+import 'package:e_commerce/features/product/data/models/product.dart';
+import 'package:e_commerce/features/product/data/models/product_details.dart';
+
+abstract class ProductRepo {
+  Future<Either<Failure, List<Product>>> getProducts();
+  Future<Either<Failure, List<Category>>> getCategories();
+  Future<Either<Failure, ProductDetails>> getProductDetails(String productId);
+  Future<Either<Failure, List<Product>>> getProductsByCategory(String category);
+  Future<Either<Failure, List<Product>>> searchProducts(String query);
+  Future<Either<Failure, List<Product>>> getRelatedProducts(String productId);
+}
