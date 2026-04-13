9fa4d877ebbf2b7708f1086e6e66f1432d811488
Osama Selim <osamaselim216@gmail.com>
2026-04-13 17:43:23 +0200

    create product feed cubit

---
+++ lib/features/home/presentation/logic/cubit/product_feed_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce/features/home/presentation/logic/cubit/product_feed_state.dart';
+import 'package:e_commerce/features/product/domain/repos/product_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class ProductFeedCubit extends Cubit<ProductFeedState> {
+  final ProductRepo _productRepo;
+  ProductFeedCubit(this._productRepo) : super(ProductFeedInitial());
+
+  Future<void> loadProducts() async {
+    emit(ProductFeedLoading());
+    final result = await _productRepo.getProducts();
+    result.fold(
+      (failure) => emit(ProductFeedError(failure.toString())),
+      (products) => emit(ProductFeedLoaded(products)),
+    );
+  }
+}
