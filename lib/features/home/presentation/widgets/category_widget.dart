004892e506d17061019ce28282193209d06026cf
Osama Selim <osamaselim216@gmail.com>
2026-04-15 23:06:19 +0200

    restore the categories list view old implementation

--- lib/features/home/presentation/widgets/category_widget.dart
+++ lib/features/home/presentation/widgets/category_widget.dart
@@ -1,51 +1,24 @@
 import 'package:e_commerce/core/helpers/spacing.dart';
+import 'package:e_commerce/features/product/data/models/category.dart';
 import 'package:flutter/material.dart';
 
 class CategoryWidget extends StatelessWidget {
-  const CategoryWidget({
-    super.key,
-    required this.name,
-    required this.image,
-    required this.onTap,
-    this.isSelected = false,
-  });
+  const CategoryWidget({super.key, required this.category});
 
-  final String name;
-  final String image;
-  final VoidCallback onTap;
-  final bool isSelected;
+  final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: SizedBox(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 200),
-              padding: const EdgeInsets.all(2.5),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: isSelected
-                      ? Theme.of(context).colorScheme.primary
-                      : Colors.transparent,
-                  width: 2.5,
-                ),
-              ),
-              child: CircleAvatar(
-                backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                backgroundImage: NetworkImage(image),
-                radius: 28,
-              ),
-            ),
-            vGap(5),
-            Text(name, overflow: TextOverflow.ellipsis),
-          ],
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: Theme.of(context).colorScheme.onPrimary,
+          backgroundImage: NetworkImage(category.imageUrl),
+          radius: 30,
         ),
-      ),
+        vGap(5),
+        Text(category.enName, overflow: .ellipsis),
+      ],
     );
   }
 }
