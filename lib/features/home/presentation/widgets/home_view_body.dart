6220fc5988a7f72eef9ef0833a445f49e582bbe4
Osama Selim <osamaselim216@gmail.com>
2026-04-27 00:30:52 +0300

    integrate the get promotions cubit in the home view

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -2,6 +2,8 @@ import 'package:e_commerce/core/helpers/constants.dart';
 import 'package:e_commerce/core/helpers/spacing.dart';
 import 'package:e_commerce/features/home/presentation/logic/categories_cubit/categories_cubit.dart';
 import 'package:e_commerce/features/home/presentation/logic/categories_cubit/categories_state.dart';
+import 'package:e_commerce/features/home/presentation/logic/get_promotions_cubit/get_promotions_cubit.dart';
+import 'package:e_commerce/features/home/presentation/logic/get_promotions_cubit/get_promotions_state.dart';
 import 'package:e_commerce/features/home/presentation/logic/product_feed_cubit/product_feed_cubit.dart';
 import 'package:e_commerce/features/home/presentation/logic/product_feed_cubit/product_feed_state.dart';
 import 'package:e_commerce/features/home/presentation/views/category_products_view.dart';
@@ -10,7 +12,6 @@ import 'package:e_commerce/features/home/presentation/widgets/custom_search_text
 import 'package:e_commerce/features/home/presentation/widgets/products_filter_sort_bar.dart';
 import 'package:e_commerce/features/home/presentation/widgets/products_grid_view.dart';
 import 'package:e_commerce/features/home/presentation/widgets/promotions_banner.dart';
-import 'package:e_commerce/features/product/data/models/promotion.dart';
 import 'package:e_commerce/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,30 +33,15 @@ class HomeViewBody extends StatelessWidget {
               children: [
                 SizedBox(height: 50, child: CustomSearchTextField()),
                 vGap(12),
-                PromotionsBanner(
-                  promotions: [
-                    Promotion(
-                      id: '1',
-                      title: 'Summer Sale',
-                      description: 'Up to 50% off on selected items',
-                      discountPercentage: 50.0,
-                      startDate: DateTime.now(),
-                      endDate: DateTime.now().add(const Duration(days: 7)),
-
-                      imageUrl:
-                          'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80',
-                    ),
-                    Promotion(
-                      id: '2',
-                      title: 'New Arrivals',
-                      description: 'Check out the latest products in our store',
-                      discountPercentage: 20.0,
-                      startDate: DateTime.now(),
-                      endDate: DateTime.now().add(const Duration(days: 7)),
-                      imageUrl:
-                          'https://images.unsplash.com/photo-1512436991641-6745cdb1723f?auto=format&fit=crop&w=800&q=80',
-                    ),
-                  ],
+                BlocBuilder<GetPromotionsCubit, GetPromotionsState>(
+                  builder: (context, state) {
+                    if (state is GetPromotionsLoaded) {
+                      return PromotionsBanner(promotions: state.promotions);
+                    } else if (state is GetPromotionsError) {
+                      return Center(child: Text(state.message));
+                    }
+                    return Center(child: CircularProgressIndicator());
+                  },
                 ),
                 vGap(10),
                 Text(
