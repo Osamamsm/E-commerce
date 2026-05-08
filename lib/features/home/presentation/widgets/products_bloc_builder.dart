5eab0827e366e35a64d0461619c529f09edc004c
Osama Selim <osamaselim216@gmail.com>
2026-05-08 16:05:07 +0300

    feat: implement ProductsErrorSliver for enhanced error display and retry functionality in product loading

--- lib/features/home/presentation/widgets/products_bloc_builder.dart
+++ lib/features/home/presentation/widgets/products_bloc_builder.dart
@@ -1,22 +1,24 @@
 import 'package:e_commerce/features/home/presentation/logic/product_feed_cubit/product_feed_cubit.dart';
 import 'package:e_commerce/features/home/presentation/logic/product_feed_cubit/product_feed_state.dart';
+import 'package:e_commerce/features/home/presentation/widgets/products_error_sliver.dart';
 import 'package:e_commerce/features/home/presentation/widgets/products_grid_view.dart';
 import 'package:e_commerce/features/product/data/models/product.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductsBlocBuilder extends StatelessWidget {
-  const ProductsBlocBuilder({
-    super.key,
-  });
+  const ProductsBlocBuilder({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ProductFeedCubit, ProductFeedState>(
       builder: (context, state) {
         if (state is ProductFeedError) {
-          return SliverToBoxAdapter(
-            child: Center(child: Text(state.message)),
+          return ProductsErrorSliver(
+            message: state.message,
+            onRetry: () {
+              context.read<ProductFeedCubit>().loadProducts();
+            },
           );
         } else if (state is ProductFeedLoaded) {
           return ProductsGridView(products: state.products);
